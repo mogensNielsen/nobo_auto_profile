1a45580425c0488b37629e2da739a71b38bfcded
Mogens Nielsen <mogensni@gmail.com>
2024-11-17 15:34:27 +0100

    Translates comments to English, removes unused code

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ tibber_query = f'''
       currentSubscription {{
         priceInfo {{
           tomorrow {{ # Prices for tomorrow
-            total # Totalpris uten nettleie
-            startsAt # Starttid for prisen
-            level # Prisnivå, f.eks. CHEAP eller NORMAL
+            total     # Total price incl. taxes but excluding net rent (nettleie)
+            startsAt  # Start time for the price
+            level     # Price level, e.g. CHEAP, NORMAL
           }}
         }}
       }}
@@ -59,6 +59,7 @@ df_tibber_prices['starts_at_time'] = df_tibber_prices['startsAt'].dt.time
 df_tibber_prices = df_tibber_prices[['total', 'starts_at_date', 'starts_at_time', 'level']]
 
 # Define the mapping dictionary
+# This is used to map between price level and mode in Nobo
 # 0: ECO
 # 1: COMFORT
 # 2: AWAY
@@ -76,7 +77,8 @@ df_prices_with_mode = df_tibber_prices.copy()
 df_prices_with_mode['mode'] = df_prices_with_mode['level'].map(level_to_mode)
 
 # The complete profile needs to have one entry for midnight pr day
-# The profile data I have is only for tomorrow so I need to a number of midnight rows before and after tomorrow
+# The profile data I have is only for tomorrow so I need to add a number of midnight
+# rows before and after tomorrow
 iso_day_number = datetime.today().isoweekday()
 nr_of_days_before_tomorrow = iso_day_number
 if iso_day_number == 7:
@@ -87,8 +89,14 @@ else:
 empty_row = [0, '2024-11-14', '00:00:00', 0, 0]
 
 # Convert the empty_row into a DataFrame with the same column names as the original DataFrame
-top_rows = pd.DataFrame([empty_row] * nr_of_days_before_tomorrow, columns=df_prices_with_mode.columns)
-bottom_rows = pd.DataFrame([empty_row] * nr_of_days_after_tomorrow, columns=df_prices_with_mode.columns)
+top_rows = pd.DataFrame(
+   [empty_row] * nr_of_days_before_tomorrow,
+   columns=df_prices_with_mode.columns
+)
+bottom_rows = pd.DataFrame(
+   [empty_row] * nr_of_days_after_tomorrow,
+   columns=df_prices_with_mode.columns
+)
 
 # Concatenate the rows at the top and bottom
 df_week_profile = pd.concat([top_rows, df_prices_with_mode, bottom_rows], ignore_index=True)
@@ -102,7 +110,7 @@ df_week_profile = df_week_profile.astype({
 })
 
 # Create the list that is needed to set a week profile
-# Each item in the list is [HHMML]
+# Each item in the list is in the format [HHMML]
 # where HH is the hour, MM is minutes and L is the mode (level) (ECO, COMFORT etc.)
 list_week_profile = [
   f'{row["starts_at_time"][:2]}{row["starts_at_time"][3:5]}{row["mode"]}'
@@ -124,35 +132,16 @@ async def main():
     # This function is called whenever the hub updates something
     def update(hub):
         print('Updating hub')
-        #print(hub.week_profiles)
 
     # Read the initial data
     update(hub)
 
-    # Try to update a week profile
-    # Something is wrong with the list, I get an error saying it should be exactly 7 entries for midnight
-    # I found out that there needs to be exactly 7 entries in the list that are `0000[mode]`. Don't know how I'm going to implement that right now
-    await hub.async_update_week_profile(week_profile_id='24', name='python_test', profile=list_week_profile)
-
-    #print(hub.week_profiles)
-    # Example of using a function
-    # zone_id 1 is the zone for the second floor
-    #print(hub.get_current_zone_mode(zone_id='1'))
-
-    # dict_zones = {}
-
-    # for key, ordered_dict in hub.zones.items():
-    #     dict_zones[key] = ordered_dict['name'].replace('\xa0', ' ')
-
-    # print(dict_zones)
-
-    # print('------')
-    # hub.create_override(
-    #     mode=nobo.API.OVERRIDE_MODE_COMFORT,
-    #     type=nobo.API.OVERRIDE_TYPE_NOW,
-    #     target_type=nobo.API.OVERRIDE_TARGET_ZONE,
-    #     target_id='1' # Zone for second floor
-    # )
+    # Update the week profile
+    await hub.async_update_week_profile(
+       week_profile_id='24',
+       name='python_test',
+       profile=list_week_profile
+    )
 
     # Listen for data updates - register before calling hub.start() to avoid race condition
     hub.register_callback(callback=update)
